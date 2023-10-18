1ab7679a44866df3f2f5c4ea245fd8f8bc3ec10b
Crondung <1922635@stud.hs-mannheim.de>
2023-10-18 14:03:19 +0200

    added onclick turn animation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './styles.scss';
+
+document
+  .getElementById('submitButton')
+  .addEventListener('click', (event) => turnCard(event, '180deg'));
+document
+  .getElementById('backButton')
+  .addEventListener('click', (event) => turnCard(event, '0deg'));
+
+function turnCard(event, deg) {
+  event.preventDefault();
+  const card = document.getElementById('card');
+  card.style.transform = `rotateY(${deg})`;
+  console.log('tuned');
+}
