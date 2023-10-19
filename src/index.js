2377ca76959f67080330a15977ae6f923867ca72
Crondung <1922635@stud.hs-mannheim.de>
2023-10-19 19:17:31 +0200

    added jsdocs

--- src/index.js
+++ src/index.js
@@ -1,25 +1,44 @@
 import { Octokit } from '@octokit/rest';
 import './styles.scss';
 
+//add event listener for the submitButton of the form
 document.getElementById('submitButton').addEventListener('click', (event) => {
   const repoNameValue = document.getElementById('repoName').value;
   const repo = findRepo(repoNameValue).then((repo) =>
     displayRepoInformation(repo)
   );
 
-  turnCard(event, '180deg');
+  turnCard(event, 180);
 });
+
+//add event listener for the return button
 document
   .getElementById('backButton')
-  .addEventListener('click', (event) => turnCard(event, '0deg'));
+  .addEventListener('click', (event) => turnCard(event, 0));
 
-function turnCard(event, deg) {
+/**
+ * Triggers the turn-animation after a click event
+ * by rotating the card by #deg degrees
+ *
+ * turning by 180 degrees will flip the card to the backside
+ * turning by 0 degrees will flip the card to the frontside
+ *
+ * @param {Event} event
+ * @param {number} deg
+ */
+export function turnCard(event, deg) {
   event.preventDefault();
   const card = document.getElementById('card');
-  card.style.transform = `rotateY(${deg})`;
+  card.style.transform = `rotateY(${deg}deg)`;
 }
 
-async function findRepo(repoName) {
+/**
+ * uses the Octokit package to retrieve repository informations
+ *
+ * @param {string} repoName
+ * @returns {Object} repoInfos with all needed fields
+ */
+export async function findRepo(repoName) {
   const octokit = new Octokit();
   try {
     const result = await octokit.rest.search.repos({
@@ -44,7 +63,13 @@ async function findRepo(repoName) {
   }
 }
 
-function displayRepoInformation(repoInformation) {
+/**
+ * Updates the backside of the card
+ * to display the repository informations
+ *
+ * @param {Object} repoInformation
+ */
+export function displayRepoInformation(repoInformation) {
   const author = document.getElementById('author-name');
   author.innerHTML = repoInformation.author;
 
